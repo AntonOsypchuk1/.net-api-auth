1494a935975b4392dbeec3831d8710213a43f109
Антон Осипчук <ipz213_oao@student.ztu.edu.ua>
2024-07-10 19:38:53 +0300

    add dtos and context dapper classes

---
+++ Dtos/UserForLoginConfirmationDto.cs
@@ -0,0 +1,8 @@
+namespace DotnetAPI.Dtos
+{
+    public class UserForLoginConfirmationDto
+    {
+        public byte[] PasswordHash {get;set;} = [];
+        public byte[] PasswordSalt {get;set;} = [];
+    }
+}
---
+++ Dtos/UserForLoginDto.cs
@@ -0,0 +1,8 @@
+namespace DotnetAPI.Dtos
+{
+    public class UserForLoginDto 
+    {
+        public string Email {get;set;} = "";
+        public string Password {get;set;} = "";
+    }
+}
